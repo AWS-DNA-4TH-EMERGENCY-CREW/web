979fcb187ce9101f40857e869850326cf8bc3d02
ECMs <youncookiexpire@kakao.com>
2022-08-20 22:06:27 +0900

    feat: device 관련 정보를 가져오고 stream정보를 생성하는 device util 추가

---
+++ src/utils/Device.ts
@@ -0,0 +1,39 @@
+export type DeviceInfo = { videoDevices: MediaDeviceInfo[]; audioDevices: MediaDeviceInfo[] }
+
+export async function handlePermissions (): Promise<DeviceInfo> {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    for (const track of stream.getTracks()) {
+        track.stop()
+
+    }
+    const devices = await navigator.mediaDevices.enumerateDevices()
+    return {
+        videoDevices: devices.filter((d) => d.kind === 'videoinput'),
+        audioDevices: devices.filter((d) => d.kind === 'audioinput')
+    }
+
+}
+
+export type StreamInfo = { microphoneStream: MediaStream; cameraStream: MediaStream }
+
+export async function getStream (deiceInfo: DeviceInfo, streamConfig : any): Promise<StreamInfo> {
+    return {
+        cameraStream: await navigator.mediaDevices.getUserMedia({
+            video: {
+                deviceId: deiceInfo.videoDevices[0].deviceId,
+                width: {
+                    ideal: streamConfig.maxResolution.width,
+                    max: streamConfig.maxResolution.width,
+                },
+                height: {
+                    ideal: streamConfig.maxResolution.height,
+                    max: streamConfig.maxResolution.height,
+                },
+            },
+        }),
+        microphoneStream: await navigator.mediaDevices.getUserMedia({
+            audio: { deviceId: deiceInfo.audioDevices[0].deviceId },
+        })
+    }
+}
+
