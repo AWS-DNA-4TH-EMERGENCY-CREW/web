d3a9463f1e4cf80cb549adcc57bee0c535194bad
ECMs <youncookiexpire@kakao.com>
2022-08-20 23:39:26 +0900

    feat: UseAPiCall hook에 skip 기능 추가

--- src/hook/UseApiCall.tsx
+++ src/hook/UseApiCall.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react'
 
 
-function useApiCall<R, T extends () => Promise<R>> (fn: T): [boolean, any | null, R | null] {
+function useApiCall<R, T extends () => Promise<R>> (fn: T, skip: boolean = false): [boolean, any | null, R | null] {
     const [isLoading, setLoading] = useState(false)
     const [error, setError] = useState<any | null>(null)
     const [data, setData] = useState<R | null>(null)
 
     useEffect(() => {
+        if (skip) {
+            return
+        }
         setLoading(true)
         fn()
             .then(res => {
@@ -19,7 +22,7 @@ function useApiCall<R, T extends () => Promise<R>> (fn: T): [boolean, any | null
             }).finally(() => {
             setLoading(false)
         })
-    }, [fn])
+    }, [fn, skip])
 
     return [isLoading, error, data]
 }
