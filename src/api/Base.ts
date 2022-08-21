c53a453a7e939aaebcf11db89abcb34ccf67dad7
ECMs <youncookiexpire@kakao.com>
2022-08-21 20:44:03 +0900

    feat: get API Base 추가 =

--- src/api/Base.ts
+++ src/api/Base.ts
@@ -1,10 +1,21 @@
 
 export const post = (url: string, body? : string) => {
+    console.log(`POST CALL ${url} \n ${JSON.stringify(body, null, 2)}`)
     return fetch(url, {
         method: 'POST',
         headers: {
             'Content-type': 'application/json'
         },
         body
+    })
+}
+
+export const get = (url: string) => {
+    console.log(`GET CALL ${url}`)
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json'
+        }
     })
 }
